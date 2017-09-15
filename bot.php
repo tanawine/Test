798bafba55ea903ba697cd6ba689ccb457617bf5
tanawine <31724534+tanawine@users.noreply.github.com>
2017-09-15 18:42:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 		
 		 $textUser = $event['message']['text'];
 		 $text1 = "ไร";
-		 $test1 = $strpos($textUser,"ดี");
+		 //$test1 = $strpos($textUser,"ดี");
 		
 		if (strpos($event['message']['text'],"ไร") ) { //&& $event['message']['text'] == "ไร"
 			// Get text sent
@@ -38,7 +38,7 @@ if (!is_null($events['events'])) {
 			$ch = curl_init($url);
 		} else if ($event['message']['text'] == "สวัสดี") {
 			// Get text sent
-			$text = "ดีครัชชชชชชชช ไร " . $event['source']['userId'].$textUser."++".$test1;
+			$text = "ดีครัชชชชชชชช ไร " . $event['source']['userId'].$textUser."กับ".$test1;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 	
@@ -68,5 +68,5 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 	}
 }
-echo "OK111";
-//echo "OK222222";
+//echo "OK111";
+echo "OK222222";
