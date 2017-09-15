e37904bd95f464b5a5ce4fa4907219c40271e393
tanawine <31724534+tanawine@users.noreply.github.com>
2017-09-15 18:13:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		 $textUser = "";
+		
 		 $textUser = $event['message']['text'];
 		 $text1 = "ไร";
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && strpos( $textUser,"ไร") ) { //&& $event['message']['text'] == "ไร"
@@ -66,5 +66,5 @@ if (!is_null($events['events'])) {
 			echo $result . "\r\n";
 	}
 }
-echo "OK111";
-//echo "OK222222";
+//echo "OK111";
+echo "OK222222";
